f1ff1d9bc3a868878365bd84f1d74a4cf26aa2dd
Omar Bsoul <omarbsoul@gmail.com>
2019-12-23 22:30:57 +0200

    adding models

---
+++ src/app/models/Payment.ts
@@ -0,0 +1,8 @@
+export class Payment {
+  id: string;
+  amount: number;
+  currencyCode: string;
+  destinationAccountNumber: number;
+  sourceAccountNumber: number;
+  paymentDescription: string;
+}
