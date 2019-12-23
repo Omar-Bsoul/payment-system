616faca2b0c81379cf30d5a1cef5696213422e23
Omar Bsoul <omarbsoul@gmail.com>
2019-12-24 00:08:43 +0200

    adding responsive navbar with spa routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { HomePageComponent } from "./components/pages/home-page/home-page.component";
+import { AccountsPageComponent } from "./components/pages/accounts-page/accounts-page.component";
+import { PaymentsPageComponent } from "./components/pages/payments-page/payments-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomePageComponent
+  },
+  {
+    path: "accounts",
+    component: AccountsPageComponent
+  },
+  {
+    path: "payments",
+    component: PaymentsPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,36 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatButtonModule } from "@angular/material";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule
+} from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+import { AccountsPageComponent } from "./components/pages/accounts-page/accounts-page.component";
+import { PaymentsPageComponent } from "./components/pages/payments-page/payments-page.component";
+import { HomePageComponent } from "./components/pages/home-page/home-page.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    AccountsPageComponent,
+    PaymentsPageComponent,
+    HomePageComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
